c38c714dcc9782b031d2911c5bd6ed3ab39f58bc
wildanhniif <whanif999@gmail.com>
2025-10-19 23:02:41 +0700

    add read, update and delete on user address routes

--- Backend/routes/addressRoutes.js
+++ Backend/routes/addressRoutes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const addressController = require('../controllers/addressController');
 
 router.post('/', addressController.createAddress);
+router.get('/user/:userId', addressController.getUserAddresses);
+router.put('/:addressId', addressController.updateAddress);
+router.delete('/:addressId', addressController.deleteAddress);
+
 
 module.exports = router;
